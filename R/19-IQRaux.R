05b1f90e46d7014daf64c03ef65435d9a8332426
karsten <karsten@IQdesktop>
2024-11-08 16:08:31 +0100

    ENH: Adding IQauxfn functions needed for mock dataset submodule

--- R/19-IQRaux.R
+++ R/19-IQRaux.R
@@ -621,12 +621,6 @@ addTminToEventTable <- function(eT, simtime) {
 }
 
 
-
-
-
-
-
-
 # Run some consistency checks for a simIQRmodelSpec
 #
 # If min(simtime) differs from min(eventTable$TIME) for any ID, the simulation might do unintended things.
@@ -677,4 +671,210 @@ checkSimSeqTimes <- function(simIQRmodelSpec,
   
 }
 
+# -------------------------------------------------------------------------#
+# Note for Reviewer ----
+# This function is from IQauxfn and can be considered validated.
+# -------------------------------------------------------------------------#
 
+#' Create an IQRdataGENEARL from a design specification
+#'
+#' Populate the data with [populateGDFWithSimValues()]
+#'
+#' @param design A list which is formatted like the `design`-argument of [IQRtools::IQRpopEDdb()]
+#'
+#' @return An [IQRtools::IQRdataGENERAL()]
+#' @export
+#' @author Daniel Lill (daniel.lill@intiquan.com)
+#' @md
+#' @family IQRdataGENERAL
+#' @family Simulation
+#' @importFrom data.table data.table rbindlist
+#'
+#' @examples
+#' design <- list(
+#'   time = list(
+#'     group1 = list(
+#'       OUTPUT1 = c(1, 2, 5, 10),
+#'       OUTPUT2 = c(1, 5, 20)
+#'     ),
+#'     group2 = list(
+#'       OUTPUT1 = c(1, 2, 5, 10),
+#'       OUTPUT2 = c(1, 5, 20)
+#'     )
+#'   ),
+#'   dosing = list(
+#'     group1 = IQRdosing(
+#'       TIME = 0, ADM = 1, AMT = 40, TINF = 0
+#'     ),
+#'     group2 = IQRdosing(
+#'       TIME = 0, ADM = 1, AMT = 40, TINF = 0
+#'     )),
+#'   groupsize = list(
+#'     group1 = 12, group2 = 3
+#'   )
+#' )
+#' create_GDF_fromDesignSpec(design)
+create_GDF_fromDesignSpec <- function(design) {
+
+  # Expand USUBJIDs
+  USUBJIDs <- lapply(setNames(nm = names(design$groupsize)), function(nm) {
+    data.table::data.table(USUBJID = paste0(nm, "_", seq_len(design$groupsize[[nm]])))
+  })
+  USUBJIDs <- data.table::rbindlist(USUBJIDs, idcol = "TRTNAME")
+
+  # Observations with reasonable default values
+  observations <- lapply(setNames(nm = names(design$time)), function(nmTRTNAME) {
+    obsTimesList <- design$time[[nmTRTNAME]]
+    x <- lapply(setNames(nm = names(obsTimesList)), function(nmOUTPUT) {
+      data.table::data.table(
+        TIME     = obsTimesList[[nmOUTPUT]],
+        TIMEUNIT = "HOURS",
+        NAME     = nmOUTPUT,
+        YTYPE    = as.numeric(gsub("OUTPUT", "", nmOUTPUT)),
+        DV       = 0,
+        VALUE    = 0,
+        UNIT     = "ug/mL",
+        LLOQ = 0, CENS = 0, EVID = 0, MDV = 0,
+        AMT = 0, ADM = NA, TINF = 0, II = 0, ADDL = 0, DURATION = 0, RATE = 0, ROUTE = NA_character_, DOSE = NA
+      )
+    })
+    data.table::rbindlist(x)
+  })
+  observations <- data.table::rbindlist(observations, idcol = "TRTNAME")
+
+  # Dosings with reasonable default values
+  dosings <- lapply(setNames(nm = names(design$dosing)), function(nm) {
+    d <- data.table::data.table(design$dosing[[nm]])
+    d[,`:=`(
+      TIMEUNIT = "HOURS",
+      NAME     = paste0("INPUT", ADM),
+      YTYPE = 0, DV = 0, VALUE = AMT, UNIT = "mg", ROUTE = "ORAL", DOSE = AMT,
+      LLOQ = NA, CENS = 0, EVID = 1, MDV = 1,
+      II = 0, ADDL = 0, DURATION = TINF, RATE = AMT/TINF)]
+  })
+  dosings <- data.table::rbindlist(dosings, idcol = "TRTNAME")
+
+  # Combine everything and create last columns
+  design0 <- data.table::rbindlist(list(dosings, observations), use.names = TRUE, fill = TRUE)
+  design0 <- merge(design0,USUBJIDs, by = "TRTNAME", allow.cartesian = TRUE)
+  design0 <- design0[order(USUBJID, TIME, YTYPE)]
+  design0 <- data.frame(design0)
+  IQRdataGENERAL(design0)
+}
+
+# -------------------------------------------------------------------------#
+# Note for Reviewer ----
+# This original function populateGDFWithSimValues is from IQauxfn and can be considered validated.
+# This function was modified to use sim_IQRmodel_consistent from IQauxfn to allow for simulation at negative time, i.e. prior to first dose
+# The modified function has not been reviewed
+# -------------------------------------------------------------------------#
+
+#' Simulate DV values and place them into a dataGENERAL
+#'
+#' The dataGEN most likely should come from [create_GDF_fromDesignSpec()].
+#' You can add individual parameters to simulate based on individuals.
+#' Simulated values will be placed into the respective VALUE and DV entries of the dataGEN.
+#'
+#' @param dataGEN An [IQRtools::IQRdataGENERAL()]
+#' @param model An [IQRtools::IQRmodel()]
+#' @param regression Character vector
+#' @param abs0inputs Character vector of abs0inputs (see [IQRtools::IQReventTable()])
+#' @param abs0Tk0param  Character vector of abs0 parameters (see [IQRtools::IQReventTable()])
+#'
+#' @return The `dataGEN` with modified DV and VALUE column
+#' @export
+#' @author Daniel Lill (daniel.lill@intiquan.com)
+#' @md
+#' @family IQRdataGENERAL
+#' @family Simulation
+#' @importFrom data.table data.table melt
+#'
+#' @examples
+#' library(ggplot2)
+#' library(IQRtools)
+#'
+#' # Step 1: Get model
+#' fileURL <- "http://192.168.1.12:7804/IQauxfn/inst/02-ExampleModels/ModelLibraryPK/model_2cpt_linsat_iv.txt"
+#' fl <- file.path(tempdir(), basename(fileURL))
+#' download.file(fileURL, fl)
+#' model <- IQRmodel(fl)
+#'
+#' # Step 2: Create empty dataGEN
+#' design <- list(
+#'   time = list(
+#'     group1 = list(
+#'       OUTPUT1 = c(1, 2, 5, 10)
+#'     ),
+#'     group2 = list(
+#'       OUTPUT1 = c(1, 2, 5, 10)
+#'     )
+#'   ),
+#'   dosing = list(
+#'     group1 = IQRdosing(
+#'       TIME = 0, ADM = 1, AMT = 40, TINF = 0
+#'     ),
+#'     group2 = IQRdosing(
+#'       TIME = 0, ADM = 1, AMT = 40, TINF = 0
+#'     )),
+#'   groupsize = list(
+#'     group1 = 10, group2 = 10
+#'   )
+#' )
+#' dataGEN <- create_GDF_fromDesignSpec(design)
+#'
+#' # Step 2.1: Add some individual parameters
+#' gpf <- IQRtools::generate_GPFfromIQRmodel(model)
+#' estimates <- gpf$estimates
+#' estimates[VALUE > 0,`:=`(IIV = 0.1)] # Update IIV Value
+#' gpf <- GPF(estimates = estimates)
+#' indPars <- sample_GPF(gpf, Nsamples = length(unique(dataGEN$ID)), FLAGid = TRUE)
+#' indPars <- indPars$indParamValues
+#' dataGEN <- merge(dataGEN, indPars, by = "ID")
+#' dataGEN <- IQRdataGENERAL(dataGEN)
+#'
+#' # Step 3: Populate dataGEN
+#' dataGENsim <- populateGDFWithSimValues(dataGEN, model, regression = setdiff(names(indPars), "ID"), abs0inputs = NULL, abs0Tk0param = NULL)
+#'
+#' # Visualize
+#' IQRggplot(dataGENsim, aes(TIME, DV, group = USUBJID)) +
+#'   facet_wrap(~TRTNAME, scales = "free") +
+#'   geom_line() +
+#'   labs(title = "Simulated data (no residual error)")
+populateGDFWithSimValues_consistent <- function(dataGEN, model, regression = NULL, abs0inputs = NULL, abs0Tk0param = NULL) {
+  # For preserving attributes of dataGEN. Nothing should change except for the values in DV and VALUE,
+  # so we can just take it as it is and replace the new attributes later.
+  atr <- attributes(dataGEN)
+
+  
+  
+  # Simulate with individual parameters and melt simres.
+  eT <- IQReventTable(data = dataGEN,
+                      regression = regression,
+                      abs0inputs = abs0inputs,
+                      abs0Tk0param = abs0Tk0param)
+  
+  # simres <- sim_IQRmodel(model, simtime = sort(unique(c(dataGEN$TIME))),
+  #                        eventTable = eT, FLAGoutputsOnly = TRUE)
+  # use sim_IQRmodelConsistent
+  simIQRmodelSpec1 <- createSimIQRmodelSpec(model, simtime = sort(unique(c(dataGEN$TIME))),
+                                            eventTable = eT, FLAGoutputsOnly = TRUE)
+  simres <- sim_IQRmodelConsistent(simIQRmodelSpec = simIQRmodelSpec1)
+  
+  simres <- data.table::data.table(simres)
+  simres <- data.table::melt(simres, id.vars = intersect(names(simres), c("ID", "TIME")),
+                             variable.name = "modelEntityId", variable.factor = FALSE, value.name = "simValue")
+  simres[,`:=`(YTYPE = as.numeric(gsub("OUTPUT", "", modelEntityId)))]
+  simres[,`:=`(modelEntityId = NULL)]
+
+  # Populate dataGEN with simulated values: Create a copy and update
+  dataGENNew <- data.table::data.table(dataGEN)
+  dataGENNew <- simres[dataGENNew, on = .NATURAL] # merge but keep row order of dataGENNe
+  dataGENNew[!is.na(simValue),`:=`(DV = simValue, VALUE = simValue)]
+  dataGENNew[,`:=`(simValue = NULL)]
+  dataGENNew <- data.frame(dataGENNew, stringsAsFactors = FALSE)
+
+  # Preserve attributes
+  dataGENNew <- IQRdataGENERAL(dataGENNew)
+  attributes(dataGENNew) <- atr
+  dataGENNew
+}
